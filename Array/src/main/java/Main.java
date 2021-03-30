9c0ce1ede43ae7fa857364318e92443bbaae5486
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-03-30 17:39:32 +0300

    test Array class functionality

--- Array/src/main/java/Main.java
+++ Array/src/main/java/Main.java
@@ -2,7 +2,16 @@ import structure.array.Array;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main class");
         Array<Integer> array = new Array<Integer>(5);
+
+        for (int i = 0; i < array.getLength(); i++) {
+            array.setItem(i, i);
+        }
+
+        for (Integer n: array) {
+            System.out.println(n);
+        }
     }
+
+
 }
