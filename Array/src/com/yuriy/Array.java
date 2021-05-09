9b9fc72e78dea3221e283d1abaae3620003378aa
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-10 01:57:22 +0300

    remove field length from Array class. use data.length instead

--- Array/src/com/yuriy/Array.java
+++ Array/src/com/yuriy/Array.java
@@ -2,23 +2,21 @@ package com.yuriy;
 
 public class Array {
     private int[] data;
-    private int length;
     private int currentIndex;
 
     public Array(int length) {
-        this.length = length;
         data = new int[length];
         currentIndex = 0;
     }
 
     public void insert(int item) {
-        if (currentIndex == length)
+        if (currentIndex == data.length)
             extendArray();
         data[currentIndex++] = item;
     }
 
     public void removeAt(int index) {
-        int[] newData = new int[--length];
+        int[] newData = new int[data.length - 1];
         for (int i = 0; i < index; i++) {
             newData[i] = data[i];
         }
@@ -37,16 +35,15 @@ public class Array {
     }
 
     private void extendArray() {
-        int[] newData = new int[length + 1];
-        for (int i = 0; i < length; i++) {
+        int[] newData = new int[data.length + 1];
+        for (int i = 0; i < data.length; i++) {
             newData[i] = data[i];
         }
         data = newData;
-        length++;
     }
 
     public int indexOf(int item) {
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == item)
                 return i;
         }
