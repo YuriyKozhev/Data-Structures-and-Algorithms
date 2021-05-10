12c63f63efebc1e4f811dc589064c951732aa675
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-10 17:12:40 +0300

    add constructor, addFirst and toString methods for Node class

--- LinkedList/src/com/yuriy/LinkedList.java
+++ LinkedList/src/com/yuriy/LinkedList.java
@@ -4,7 +4,29 @@ public class LinkedList {
     private Node first;
     private Node last;
 
-    // addFirst
+    public LinkedList() {
+        last = new Node(0, null);
+        first = new Node(0, last);
+    }
+
+    public void addFirst(int value) {
+        first.setValue(value);
+        first = new Node(0, first);
+    }
+
+    @Override
+    public String toString() {
+        String s = "[";
+        Node current = first.getNext();
+        while (current != last) {
+            s += current.getValue();
+            s += ", ";
+            current = current.getNext();
+        }
+        s += "]";
+        return s;
+    }
+
     // addLast
     // deleteFirst
     // deleteLast
