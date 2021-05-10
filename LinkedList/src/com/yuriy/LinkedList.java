c14779690969e7c825e5aed265d0de0f4c1e9f9a
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-10 19:06:52 +0300

    reimplement methods to introduce new logic: first and last will be parts of the linked list itself

--- LinkedList/src/com/yuriy/LinkedList.java
+++ LinkedList/src/com/yuriy/LinkedList.java
@@ -7,6 +7,7 @@ public class LinkedList {
 
         public Node(int value) {
             this.value = value;
+            this.next = null;
         }
 
         public Node(int value, Node next) {
@@ -19,20 +20,21 @@ public class LinkedList {
     private Node last;
 
     public LinkedList() {
-        last = new Node(0, null);
-        first = new Node(0, last);
+        first = last = null;
     }
 
     public void addFirst(int value) {
-        first.value = value;
-        first = new Node(0, first);
+        if (isEmpty())
+            first = last = new Node(value);
+        else
+            first = new Node(value, first);
     }
 
     @Override
     public String toString() {
         String s = "[";
-        Node current = first.next;
-        while (current != last) {
+        Node current = first;
+        while (current != null) {
             s += current.value;
             s += ", ";
             current = current.next;
@@ -42,45 +44,49 @@ public class LinkedList {
     }
 
     public void addLast(int value) {
-        last.value = value;
-        Node newLast = new Node(0, null);
-        last.next = newLast;
-        last = newLast;
+//        last.value = value;
+//        Node newLast = new Node(0, null);
+//        last.next = newLast;
+//        last = newLast;
     }
 
     public void deleteFirst() {
-        first = first.next;
-        first.value = 0;
+//        first = first.next;
+//        first.value = 0;
     }
 
     public void deleteLast() {
-        Node secondToLast = first.next;
-        while (secondToLast.next != last)
-            secondToLast = secondToLast.next;
-        secondToLast.next = null;
-        secondToLast.value = 0;
-        last = secondToLast;
+//        Node secondToLast = first.next;
+//        while (secondToLast.next != last)
+//            secondToLast = secondToLast.next;
+//        secondToLast.next = null;
+//        secondToLast.value = 0;
+//        last = secondToLast;
     }
 
     public boolean contains(int value) {
-        Node current = first.next;
-        while (current != last) {
-            if (current.value == value)
-                return true;
-            current = current.next;
-        }
+//        Node current = first.next;
+//        while (current != last) {
+//            if (current.value == value)
+//                return true;
+//            current = current.next;
+//        }
         return false;
     }
 
     public int indexOf(int value) {
-        Node current = first.next;
-        int i = 0;
-        while (current != last) {
-            if (current.value == value)
-                return i;
-            current = current.next;
-            i++;
-        }
+//        Node current = first.next;
+//        int i = 0;
+//        while (current != last) {
+//            if (current.value == value)
+//                return i;
+//            current = current.next;
+//            i++;
+//        }
         return -1;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
     }
 }
