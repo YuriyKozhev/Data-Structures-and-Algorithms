8b89104eafbf852440b5a699d11d177829088d27
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-10 15:50:05 +0300

    create skeleton for self-implemented LinkedList

---
+++ LinkedList/src/com/yuriy/LinkedList.java
@@ -0,0 +1,13 @@
+package com.yuriy;
+
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+}
