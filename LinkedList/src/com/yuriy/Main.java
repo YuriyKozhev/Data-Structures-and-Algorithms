dcb49c8807a74883e373d7613a782d341bd71d3a
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-10 17:13:47 +0300

    test LinkedList addFirst and toString methods

--- LinkedList/src/com/yuriy/Main.java
+++ LinkedList/src/com/yuriy/Main.java
@@ -3,6 +3,9 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedListDemo.show();
+        LinkedList list = new LinkedList();
+        list.addFirst(10);
+        list.addFirst(20);
+        System.out.println(list);
     }
 }
