ea7b3734628bda2c83e2a97ae7bbbb55cf512833
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-11 00:19:07 +0300

    add StackDemo class

---
+++ Stack/src/com/yuriy/StackDemo.java
@@ -0,0 +1,18 @@
+package com.yuriy;
+
+import java.util.Stack;
+
+public class StackDemo {
+    public static void show() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
+    }
+}
