93db175c781fdebfa5d08d7922528c6c5e8066ef
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-13 00:47:16 +0300

    create ArrayImplementedStack class and its demo

---
+++ Stack/src/com/yuriy/ArrayImplementedStack.java
@@ -0,0 +1,23 @@
+package com.yuriy;
+
+public class ArrayImplementedStack {
+    public ArrayImplementedStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+}
